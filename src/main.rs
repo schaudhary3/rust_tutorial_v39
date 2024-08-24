196b8ded3fabe2df54b07e9ad1fe36ec8c21bf2b
cs99alpha <somendrac90@gmail.com>
2024-08-24 18:28:28 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+//use std::iter::Map;
+//use rocket::Response::NamedFile;
+use rocket::fs::NamedFile;
+use rocket::get;
+use std::path::{Path, PathBuf};
+
+//use rocket::http::RawStr;
+#[macro_use] extern crate rocket;
+
+/*#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}*/
+
+#[get("/hello/<name>")]
+fn hello(name: /*&RawStr*/String) -> String {
+    format!("Hello, {}!", name.as_str())
+}
+
+#[get("/health_check")]
+fn health_check() -> &'static str {
+    "OK"
+}
+
+#[get("/file/<file_name>")]
+async fn get_file(file_name: String) -> Option<NamedFile> {
+    //Construct the path to the file
+    let path: PathBuf = Path::new("file").join(file_name);
+
+    //Return the file if it exists, otherwsise return None
+    NamedFile::open(path).await.ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
+    .mount("/", routes![health_check])
+    .mount("/", routes![get_file])
+}
+
+//let data = Map
